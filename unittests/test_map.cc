72544fd12487f0b18b35afe7011c0d937fe4a943
iivlev <iivlev@productengine.com>
2017-05-25 22:37:39 +0300

    Few more map tests

--- unittests/test_map.cc
+++ unittests/test_map.cc
@@ -13,54 +13,79 @@ using namespace map_solver;
 const int kMapWidth = 10;
 const int kMapHeight = 10;
 
+const char kTestField[] =
+    "s         "
+    "          "
+    "          "
+    " ******   "
+    "          "
+    "          "
+    "          "
+    "          "
+    "          "
+    "        *e";
+
 class MapTest : public ::testing::Test {
 protected:
     virtual void SetUp() override {
         Logging::initLogging();
-        m_map = make_unique<Map>();
     }
     virtual void TearDown() override {  }
-    std::unique_ptr<Map> m_map;
+    Map m_map;
 };
 
 TEST_F(MapTest, InitialWidthHeight) {
-    ASSERT_EQ(m_map->width(), -1);
-    ASSERT_EQ(m_map->height(), -1);
+    EXPECT_EQ(m_map.width(), -1);
+    EXPECT_EQ(m_map.height(), -1);
 }
 
 TEST_F(MapTest, ExpectedWidthHeight) {
-    m_map->readFromFile(GTEST_MAP_FILE);
+    m_map.readFromFile(GTEST_MAP_FILE);
 
-    ASSERT_EQ(m_map->width(), kMapWidth);
-    ASSERT_EQ(m_map->height(), kMapHeight);
+    ASSERT_EQ(m_map.width(), kMapWidth);
+    ASSERT_EQ(m_map.height(), kMapHeight);
 }
 
 TEST_F(MapTest, ExpectedCellCount) {
-    ASSERT_EQ(m_map->readFromFile(GTEST_MAP_FILE), kMapWidth * kMapHeight);
+    ASSERT_EQ(m_map.readFromFile(GTEST_MAP_FILE), kMapWidth * kMapHeight);
 }
 
 TEST_F(MapTest, ReadFromStream) {
-    vector<char> v;
     stringstream stream;
 
-    const char field[] =
-        "s         "
-        "          "
-        "          "
-        "          "
-        "          "
-        "          "
-        "          "
-        "          "
-        "          "
-        "         e"
-    ;
     uint32_t ten = 10;
     stream.write((const char*)&ten, sizeof(uint32_t));
     stream.write((const char*)&ten, sizeof(uint32_t));
-    stream.write(field, sizeof(field) - 1);
+    stream.write(kTestField, sizeof(kTestField) - 1);
 
-    ASSERT_EQ(sizeof(field), 101);
-    ASSERT_EQ(m_map->readFromStream(stream), 100);
+    ASSERT_EQ(sizeof(kTestField), 101);
+    ASSERT_EQ(m_map.readFromStream(stream), 100);
 }
 
+TEST_F(MapTest, StartFinish) {
+    stringstream stream;
+    uint32_t ten = 10;
+    stream.write((const char*)&ten, sizeof(uint32_t));
+    stream.write((const char*)&ten, sizeof(uint32_t));
+    stream.write(kTestField, sizeof(kTestField) - 1);
+
+    ASSERT_EQ(m_map.readFromStream(stream), 100);
+    ASSERT_EQ(m_map.start(), 0);
+    ASSERT_EQ(m_map.finish(), 99);
+}
+
+TEST_F(MapTest, Weight) {
+    stringstream stream;
+    uint32_t ten = 10;
+    stream.write((const char*)&ten, sizeof(uint32_t));
+    stream.write((const char*)&ten, sizeof(uint32_t));
+    stream.write(kTestField, sizeof(kTestField) - 1);
+
+    ASSERT_EQ(m_map.readFromStream(stream), 100);
+
+    ASSERT_EQ(m_map.weight(m_map.start()), 1);
+    ASSERT_EQ(m_map.weight(m_map.finish()), 1);
+
+    int32_t cellWithWall = m_map.finish() - 1;
+    EXPECT_GT(m_map.weight(cellWithWall), 50000);
+}
