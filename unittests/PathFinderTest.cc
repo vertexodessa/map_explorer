f00333f7f6a9a3f0a6c0c8b31d4f5634a083caa6
iivlev <iivlev@productengine.com>
2017-05-28 11:55:07 +0300

    Parameterized tests for path finders

--- unittests/PathFinderTest.cc
+++ unittests/PathFinderTest.cc
@@ -24,32 +24,45 @@ const char kTestField[] =
     "          "
     "        *e";
 
-class DijkstraPFTest : public ::testing::Test {
+class PFTest : public ::testing::TestWithParam<PathFinderFactory::PathFinderType> {
 protected:
     virtual void SetUp() override { }
     virtual void TearDown() override { }
 };
 
-TEST_F(DijkstraPFTest, SolvedMapWeight) {
+INSTANTIATE_TEST_CASE_P(PathFinderType,
+                        PFTest,
+                        ::testing::Values(PathFinderFactory::AStar, PathFinderFactory::Dijkstra));
+
+namespace {
+shared_ptr<Map> MapFromString(index_t width, index_t height, const string& s) {
     stringstream ss;
-    uint32_t ten = 10;
-    ss.write((const char*)&ten, sizeof(ten));
-    ss.write((const char*)&ten, sizeof(ten));
-    ss.write(kTestField, sizeof(kTestField) - 1);
+
+    ss.write((const char*)&width, sizeof(width));
+    ss.write((const char*)&height, sizeof(height));
+    ss.write(s.data(), s.length());
 
     shared_ptr<Map> map = make_shared<Map>();
-    ASSERT_NE(kInvalidIndex, map->readFromStream(ss));
+    index_t cell_count = map->readFromStream(ss);
+    EXPECT_NE(kInvalidIndex, cell_count);
+
+    return map;
+}
+}
+
+TEST_P(PFTest, SolvedMapWeight) {
 
     PathFinderFactory f;
-    f.setType(PathFinderFactory::Dijkstra);
+    f.setType(GetParam());
 
+    shared_ptr<Map> map = MapFromString(10, 10, kTestField);
     unique_ptr<IPathFinder> pf = f.create(map);
     unique_ptr<Path> path { pf->solve() };
 
     EXPECT_EQ(path->weight(), 18);
 }
 
-TEST_F(DijkstraPFTest, UnreachableDestination) {
+TEST_P(PFTest, UnreachableDestination) {
     const char kLocalTestField[] =
         "s*        "
         "**        "
@@ -62,23 +75,16 @@ TEST_F(DijkstraPFTest, UnreachableDestination) {
         "          "
         "        *e";
 
-    stringstream ss;
-    uint32_t ten = 10;
-    ss.write((const char*)&ten, sizeof(ten));
-    ss.write((const char*)&ten, sizeof(ten));
-    ss.write(kLocalTestField, sizeof(kLocalTestField) - 1);
-
-    shared_ptr<Map> map = make_shared<Map>();
-    ASSERT_NE(kInvalidIndex, map->readFromStream(ss));
+    shared_ptr<Map> map = MapFromString(10, 10, kLocalTestField);
 
     PathFinderFactory f;
-    f.setType(PathFinderFactory::Dijkstra);
+    f.setType(GetParam());
 
     unique_ptr<IPathFinder> pf = f.create(map);
     ASSERT_THROW(pf->solve(), DestinationUnreachableException);
 }
 
-TEST_F(DijkstraPFTest, DoubleWallsAreStillUnreachable) {
+TEST_P(PFTest, DoubleWallsAreStillUnreachable) {
     const char kLocalTestField[] =
         "s*        "
         "**        "
@@ -91,17 +97,10 @@ TEST_F(DijkstraPFTest, DoubleWallsAreStillUnreachable) {
         "          "
         "        *e";
 
-    stringstream ss;
-    uint32_t ten = 10;
-    ss.write((const char*)&ten, sizeof(ten));
-    ss.write((const char*)&ten, sizeof(ten));
-    ss.write(kLocalTestField, sizeof(kLocalTestField) - 1);
-
-    shared_ptr<Map> map = make_shared<Map>();
-    ASSERT_NE(kInvalidIndex, map->readFromStream(ss));
+    shared_ptr<Map> map = MapFromString(10, 10, kLocalTestField);
 
     PathFinderFactory f;
-    f.setType(PathFinderFactory::Dijkstra);
+    f.setType(GetParam());
 
     unique_ptr<IPathFinder> pf = f.create(map);
     ASSERT_THROW(pf->solve(), DestinationUnreachableException);
